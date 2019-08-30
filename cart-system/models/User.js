3979102c14e8f155fee48425f8972ecbe5bced10
Jain-Sanchit <Jainsanchit157@gmail.com>
2019-08-31 02:31:57 +0530

    plantsCart

--- cart-system/models/User.js
+++ cart-system/models/User.js
@@ -17,10 +17,6 @@ const userSchema=mongoose.Schema({
         type: String,
         required: true
 
-      },
-      date: {
-        type: Date,
-        default: Date.now
       }
 });
 
--- cart-system/routes/index.js
+++ cart-system/routes/index.js
@@ -2,14 +2,20 @@ const express=require('express')
 
 const router=express.Router()
 router.use(express.urlencoded({extended: true}))
+const item=require('../models/User');
+
+item.find(()=>{
+    console.log(name);
+    
+})
+
 router.get('/',(req,res)=>{
     res.render('home')
 })
 
 router.get('/dashboard',(req,res)=>{
-    res.render('dashboard',{
-        item
-    })
+    res.render('dashboard',{item});
 })
+
 
 module.exports = router;
